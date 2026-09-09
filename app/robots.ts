1de4efbd96a9b8edfe4e6f48da038396b1db0862
alvinaug1105 <105107561+alvinaug1105@users.noreply.github.com>
2026-09-09 08:15:35 +0800

    Delete app directory

--- app/robots.ts
+++
@@ -1,7 +0,0 @@
-import type { MetadataRoute } from 'next';
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: '*', allow: '/', disallow: ['/api/', '/admin'] },
-    sitemap: 'https://rls-website.alvin1105alvin.workers.dev/sitemap.xml',
-  };
-}
